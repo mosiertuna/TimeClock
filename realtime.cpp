ede3e8d606c43af779bbeac5dc7557447b3a9be5
Mạc Thanh Bình <115092045+mosiertuna@users.noreply.github.com>
2024-05-21 12:08:52 +0700

    giai đoạn tìm hiểu và phát triển tính năng

--- realtime.cpp
+++ realtime.cpp
@@ -1,7 +1,23 @@
 #include <stdio.h>
 
+prime(int n) {
+    int i;
+    for (i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+
+
 
 int main() {
-    printf("Hello, World!\n");
+    int n;
+    scanf("%d", &n);
+    printf("%d\n", prime(n));
+    
+
     return 0;
 }
