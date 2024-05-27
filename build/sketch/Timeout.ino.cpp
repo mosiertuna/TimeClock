23490afc0d307421a4dd1260eeb45338c9bf44d4
Mạc Thanh Bình <115092045+mosiertuna@users.noreply.github.com>
2024-05-27 20:06:54 +0700

    thử nghiệm

---
+++ build/sketch/Timeout.ino.cpp
@@ -0,0 +1,27 @@
+#include <Arduino.h>
+#line 1 "D:\\84333\\Documents\\New folder\\TimeClock\\Timeout\\Timeout.ino"
+#include <Servo.h>
+
+static const int servoPin = 13;
+
+Servo servo1;
+
+void setup() {
+  Serial.begin(115200);
+  servo1.attach(servoPin);
+}
+
+void loop() {
+  for(int posDegrees = 0; posDegrees <= 180; posDegrees++) {
+    servo1.write(posDegrees);
+    Serial.println(posDegrees);
+    delay(20);
+  }
+
+  for(int posDegrees = 180; posDegrees >= 0; posDegrees--) {
+    servo1.write(posDegrees);
+    Serial.println(posDegrees);
+    delay(20);
+  }
+}
+
